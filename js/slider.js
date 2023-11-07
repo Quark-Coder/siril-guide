c987829353a323f1fba66730f072330143616e5b
Quark <64560104+Quark-Coder@users.noreply.github.com>
2023-11-07 13:28:29 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $("input.slider").on("input change", function(event) {
+        var element = $(this).parents("div.slider_container");
+        var pos = event.target.value;
+
+        element.find("div.before").css({width: pos + "%"});
+        element.find("div.slider-button").css({left: "calc(" + pos + "% - 18px)"});
+    });
+
+});
